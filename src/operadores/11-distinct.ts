dc570a52fbed43f142aca6dab004de4fc64f84d7
Jose Rinaldi <jrinaldi@mobydigital.com>
2024-07-09 09:56:30 -0300

    modificacion en operadores deprecados

--- src/operadores/11-distinct.ts
+++ src/operadores/11-distinct.ts
@@ -3,7 +3,8 @@ import { distinct } from 'rxjs/operators';
 
 
 
-const numeros$ = of<number|string>(1,'1',1,3,3,2,2,4,4,5,3,1, '1' );
+//const numeros$ = of<number|string>(1,'1',1,3,3,2,2,4,4,5,3,1, '1' );
+const numeros$ = of(1,'1',1,3,3,2,2,4,4,5,3,1, '1');
 
 numeros$.pipe(
     distinct() // ===
